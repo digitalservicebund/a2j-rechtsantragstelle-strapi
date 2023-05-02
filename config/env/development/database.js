7964dd7b10e08726a23d7834baec9d617a47f45b
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2023-05-02 15:35:44 +0200

    Update trim for security infos

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,13 +1,13 @@
-module.exports =  ({ env }) => ({
-	connection: {
-		client: 'postgres',
-		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 5431),
-			database: env('DATABASE_NAME', 'strapi'),
-			user: env('DATABASE_USERNAME', 'strapi'),
-			password: env('DATABASE_PASSWORD', 'STRAPI'),
-			ssl: env.bool('DATABASE_SSL', false)
-		}
-	}
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5431),
+      database: env('DATABASE_NAME', 'strapi').trim(),
+      user: env('DATABASE_USERNAME', 'strapi').trim(),
+      password: env('DATABASE_PASSWORD', 'STRAPI').trim(),
+      ssl: env.bool('DATABASE_SSL', false)
+    }
+  }
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,13 @@
-module.exports =  ({ env }) => ({
-	connection: {
-		client: 'postgres',
-		connection: {
-		  host: env('DATABASE_HOST', ''),
-			port: env.int('DATABASE_PORT', undefined),
-			database: env('DATABASE_NAME', ''),
-			user: env('DATABASE_USERNAME', ''),
-			password: env('DATABASE_PASSWORD', ''),
-			ssl: env.bool('DATABASE_SSL', false)
-		}
-	}
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', ''),
+      port: env.int('DATABASE_PORT', undefined),
+      database: env('DATABASE_NAME', '').trim(),
+      user: env('DATABASE_USERNAME', '').trim(),
+      password: env('DATABASE_PASSWORD', '').trim(),
+      ssl: env.bool('DATABASE_SSL', false)
+    }
+  }
 });
