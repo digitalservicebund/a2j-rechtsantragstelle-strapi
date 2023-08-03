808549beb5704befdbdf8a14e418bee9e7210025
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-08-03 11:04:17 +0200

    Migrate button field to buttons field
    co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
    co-authored-by: chris <christoph.hohnerlein@digitalservice.bund.de>
    co-authored-by: Frederike Ramin <frederike.ramin@digitalservice.bund.de>

---
+++ database/migrations/2023.08.03T00.00.00.move-button-to-buttons.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/**
+ * what this migration is doing:
+ * if button exists and no buttons -> make the one button the first of the buttons
+ *
+ * what we need to do afterwards:
+ * delete the button property from the schemas -> will remove the column from the db
+ *
+ */
+
+async function up(knex) {
+  console.log("move button to buttons");
+
+  const components_with_button_and_buttons = [
+    "boxes",
+    "link_list_boxes",
+    "info_box_items",
+  ];
+
+  let changed = 0;
+
+  for (const component of components_with_button_and_buttons) {
+    const componentIds = await knex(`components_page_${component}`).select(
+      "id"
+    );
+    for (const { id: componentId } of componentIds) {
+      const nestedComponents = await knex(
+        `components_page_${component}_components`
+      )
+        .select("*")
+        .where({ entity_id: componentId });
+      const buttonExists = nestedComponents.some(
+        (row) => row.field === "button"
+      );
+      const buttonsExist = nestedComponents.some(
+        (row) => row.field === "buttons"
+      );
+
+      if (buttonExists && !buttonsExist) {
+        // change field "button" to "buttons"
+        const changedDatasetCount = await knex(
+          `components_page_${component}_components`
+        )
+          .where({ entity_id: componentId, field: "button" })
+          .update({ field: "buttons" });
+        changed += changedDatasetCount;
+        console.log({ changedDatasetCount, componentId, component });
+      }
+    }
+  }
+  console.log({ changed });
+}
+
+module.exports = { up };
