701af245b71ccbda4147f00e3cf5dd808d596ff9
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2023-09-14 12:58:30 +0200

    feat(publish): add site-publisher plugin to trigger GH CI action
    co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = ({ env }) => {
-  const config = {};
+  const config = {
+    "site-publisher": {
+      config: {
+        owner: "digitalservicebund",
+        repo: "a2j-rechtsantragstelle",
+        workflow_id: "ci-pipeline.yml",
+        token: env("GITHUB_TOKEN", "dummy"),
+        branch: "main",
+        inputs: {},
+      },
+    },
+  };
   if (env("OBS_ENDPOINT") && env("OBS_BUCKET_NAME")) {
     Object.assign(config, {
       upload: {
