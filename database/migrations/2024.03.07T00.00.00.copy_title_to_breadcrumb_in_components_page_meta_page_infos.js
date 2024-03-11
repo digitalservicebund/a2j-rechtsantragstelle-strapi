a580e025cb3e3c991ae7ccbed8409c9ac9e6c1d6
Rafael Falk <rafael.falk@digitalservice.bund.de>
2024-03-11 14:42:36 +0100

    fix(migrations): add try catch block
    Co-authored-by: Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>

--- database/migrations/2024.03.07T00.00.00.copy_title_to_breadcrumb_in_components_page_meta_page_infos.js
+++ database/migrations/2024.03.07T00.00.00.copy_title_to_breadcrumb_in_components_page_meta_page_infos.js
@@ -6,35 +6,46 @@ It copies the title to the breadcrumb in components_page_meta_page_infos
 */
 
 async function up(knex) {
-  let counter = 0;
+  try {
+    let counter = 0;
 
-  const metaPageInfosWithoutBreadcrumb = await knex(
-    "components_page_meta_page_infos"
-  ).select("id", "title");
+    const metaPageInfosWithoutBreadcrumb = await knex(
+      "components_page_meta_page_infos"
+    ).select("id", "title");
 
-  if (
-    await knex.schema.hasColumn("components_page_meta_page_infos", "breadcrumb")
-  ) {
-    metaPageInfosWithoutBreadcrumb.whereNull("breadcrumb");
-    console.log(
-      "Selecting non-null breadcrumb rows from components_page_meta_page_infos"
-    );
-  } else {
-    await knex.schema.alterTable("components_page_meta_page_infos", (table) => {
-      table.string("breadcrumb").notNullable().defaultTo("");
-    });
-    console.log("Added breadcrumb column to components_page_meta_page_infos");
+    if (
+      await knex.schema.hasColumn(
+        "components_page_meta_page_infos",
+        "breadcrumb"
+      )
+    ) {
+      metaPageInfosWithoutBreadcrumb.whereNull("breadcrumb");
+      console.log(
+        "Selecting non-null breadcrumb rows from components_page_meta_page_infos"
+      );
+    } else {
+      await knex.schema.alterTable(
+        "components_page_meta_page_infos",
+        (table) => {
+          table.string("breadcrumb").notNullable().defaultTo("");
+        }
+      );
+      console.log("Added breadcrumb column to components_page_meta_page_infos");
+    }
+
+    for (const { id, title } of metaPageInfosWithoutBreadcrumb) {
+      const breadcrumb = title || "";
+      await knex("components_page_meta_page_infos").where({ id }).update({
+        breadcrumb,
+      });
+      console.log(`Updated breadcrumb with value ${breadcrumb} for id ${id}`);
+      counter++;
+    }
+    console.log(`Updated ${counter} rows`);
+  } catch (error) {
+    console.log("error occured during migration. Skipping migration");
+    console.log(error);
   }
-
-  for (const { id, title } of metaPageInfosWithoutBreadcrumb) {
-    const breadcrumb = title || "";
-    await knex("components_page_meta_page_infos").where({ id }).update({
-      breadcrumb,
-    });
-    console.log(`Updated breadcrumb with value ${breadcrumb} for id ${id}`);
-    counter++;
-  }
-  console.log(`Updated ${counter} rows`);
 }
 
 module.exports = { up };
