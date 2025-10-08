1eef6475d58731e2055a3a2e268be247cc71eed8
aaschlote <alex.schlote@digitalservice.bund.de>
2025-10-08 15:08:34 +0200

    refactor: update dependencies and enable koa proxy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+  proxy: { koa: true },
 });
